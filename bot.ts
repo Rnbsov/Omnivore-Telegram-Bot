4484f45cd5dcba28f382eadb4113697f26013066
Rnbsov <rnbsov@gmail.com>
2024-01-06 22:26:56 +0600

    ☄ Connect free database

--- bot.ts
+++ bot.ts
@@ -1,26 +1,35 @@
 import { load } from 'https://deno.land/std@0.211.0/dotenv/mod.ts'
 import { Bot } from 'https://deno.land/x/grammy@v1.20.2/mod.ts'
 import { mainKeyboardLayout } from './src/keyboards.ts'
-import { MyContext, sessionHandler } from "./src/sessionsHandler.ts";
+import { MyContext, sessionHandler } from './src/sessionsHandler.ts'
 
 const env = await load()
 
 const bot = new Bot<MyContext>(env['BOT_TOKEN'])
 
-bot.use(sessionHandler())
+bot.use(sessionHandler(bot.token))
 
 bot.command('start', ctx =>
   ctx.reply(
     'Welcome! Up and running. \n\n<b>👾 Save a bunch</b> - allows you to save a bunch of urls at once',
     {
       reply_markup: mainKeyboardLayout,
-      parse_mode: "HTML"
+      parse_mode: 'HTML',
     }
   )
 )
 
+bot.hears('set', ctx => {
+  ctx.session.apiToken = 'smth'
+  ctx.reply('set new token')
+})
+
+bot.hears('get', ctx => {
+  const token = ctx.session.apiToken
+  ctx.reply(`your token ${token}`)
+})
+
 bot.hears('👾 Save a bunch of urls', ctx => {
-  
   ctx.reply(`Send me urls in following format:
   
   url1
--- src/sessionsHandler.ts
+++ src/sessionsHandler.ts
@@ -1,8 +1,10 @@
+import { load } from 'https://deno.land/std@0.211.0/dotenv/mod.ts'
 import {
   Context,
   session,
   SessionFlavor,
 } from 'https://deno.land/x/grammy@v1.20.2/mod.ts'
+import { freeStorage } from 'https://deno.land/x/grammy_storages/free/src/mod.ts'
 
 interface SessionData {
   apiToken: string
@@ -12,10 +14,13 @@ export type MyContext = Context & SessionFlavor<SessionData>
 
 function initial(): SessionData {
   return {
-    apiToken: '',
+    apiToken: ''
   }
 }
 
-export const sessionHandler = () => {
-  return session({ initial })
+export const sessionHandler = (token: string) => {
+  return session({
+    initial,
+    storage: freeStorage<SessionData>(token),
+  })
 }

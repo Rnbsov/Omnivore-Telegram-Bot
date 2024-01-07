c0f9fec529681631d5dd8e1e745b35bbc1b432a2
Rnbsov <rnbsov@gmail.com>
2024-01-07 18:52:02 +0600

    supabase

--- src/sessionsHandler.ts
+++ src/sessionsHandler.ts
@@ -10,6 +10,12 @@ import { ConversationFlavor } from "https://deno.land/x/grammy_conversations@v1.
 
 const env = await load()
 
+console.log(Deno.env.get('SUPABASE_URL'))
+console.log()
+console.log(Deno.env.get('SUPABASE_KEY'))
+
+console.log(env['BOT_TOKEN'])
+
 const supabase = createClient(
   env['SUPABASE_URL'],
   env['SUPABASE_KEY']

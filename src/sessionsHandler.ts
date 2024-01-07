7bb39b4d96a9e63d92a5fca13c5d679a1d5ee014
Rnbsov <rnbsov@gmail.com>
2024-01-07 18:43:50 +0600

    🍏 Supabase version bump

--- src/sessionsHandler.ts
+++ src/sessionsHandler.ts
@@ -3,7 +3,7 @@ import {
   session,
   SessionFlavor,
 } from 'https://deno.land/x/grammy@v1.20.2/mod.ts'
-import { createClient } from 'https://esm.sh/@supabase/supabase-js@1.24.0'
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2.33.1"
 import { supabaseAdapter } from 'https://deno.land/x/grammy_storages@v2.4.1/supabase/src/mod.ts'
 import { load } from 'https://deno.land/std@0.211.0/dotenv/mod.ts'
 import { ConversationFlavor } from "https://deno.land/x/grammy_conversations@v1.2.0/conversation.ts";

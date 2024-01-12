00859adfde7b6ace7dd8f0756d67048eba1a34b0
Rnbsov <rnbsov@gmail.com>
2024-01-12 20:04:33 +0600

    🍃 Inline query logic

--- src/inlineQuery.ts
+++ src/inlineQuery.ts
@@ -1,8 +1,25 @@
-import { Composer } from "https://deno.land/x/grammy@v1.20.3/mod.ts";
+import { Composer } from 'https://deno.land/x/grammy@v1.20.3/mod.ts'
+import { OmnivoreApi } from './omnivore/api.ts'
+import { MyContext } from './sessionsHandler.ts'
 
-export const inlineQuery = new Composer()
+export const inlineQuery = new Composer<MyContext>()
 
-inlineQuery.on('inline_query', ctx => {
+inlineQuery.on('inline_query', async ctx => {
   const query = ctx.inlineQuery.query
-  console.log(query)
+  const token = ctx.session.apiToken
+
+  const api = new OmnivoreApi(token)
+
+  const offset = ctx.inlineQuery.offset || ''
+
+  const { results, nextOffset } = await api.search(query, offset)
+
+  console.log(results)
+  console.log()
+  console.log(nextOffset)
+  
+  await ctx.answerInlineQuery(results, {
+    cache_time: 300,
+    next_offset: nextOffset,
+  })
 })

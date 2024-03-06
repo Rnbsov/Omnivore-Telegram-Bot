6f149d323c562912cb194f79d7d034bb3874e5e7
Rnbsov <rnbsov@gmail.com>
2024-03-06 23:57:57 +0500

    🍋 Add function to get the label with source name to utils

---
+++ src/utils/getSourceLabel.ts
@@ -0,0 +1,27 @@
+import { MessageOrigin } from "https://deno.land/x/grammy_types@v3.4.6/message.ts";
+
+export function getSourceLabel(source: MessageOrigin) {
+  let sourceLabel
+  
+  if (source) {
+    switch (source.type) {
+      case 'user':
+        sourceLabel = { name: `${source.sender_user.first_name} ${source.sender_user.last_name}` }
+        break;
+      case 'hidden_user':
+        sourceLabel = { name: source.sender_user_name } 
+        break
+      case 'channel':
+        sourceLabel = { name: source.chat.title } 
+        break
+      case 'chat':
+        sourceLabel = { name: source.author_signature || '' } 
+        break
+      default:
+        sourceLabel = {}
+        break;
+    }
+
+    return sourceLabel
+  }
+}

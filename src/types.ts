606f69a9dc26abdea822e8b090aa0fd31cdca557
Rnbsov <rnbsov@gmail.com>
2024-03-08 19:13:05 +0500

    🍊 Update parseUrls to put label in specific objects

---
+++ src/types.ts
@@ -0,0 +1,8 @@
+type label = {
+  name: string
+}
+
+export type UrlInfo = {
+  url: string
+  labels: label[]
+}
--- src/utils/parseUrls.ts
+++ src/utils/parseUrls.ts
@@ -1,4 +1,4 @@
-import { UrlInfo } from './types.ts'
+import { UrlInfo } from '../types.ts'
 
 export function parseUrls(urls: string): UrlInfo[] {
   const regex = /((?:https?:\/\/|www\.)[^ \n\r]+)(.*)/g
@@ -8,7 +8,7 @@ export function parseUrls(urls: string): UrlInfo[] {
 
   while ((match = regex.exec(urls)) !== null) {
     const url = match[1].trim()
-    const labels = match[2] ? match[2].trim().split(/\s+/) : []
+    const labels = match[2] ? match[2].trim().split(/\s+/).map(label => ({ name: label })) : []
 
     urlsArray.push({ url, labels })
   }

e0337ed520b9fb2d978174e78966ed16d63fec7e
Rnbsov <rnbsov@gmail.com>
2024-03-08 15:36:26 +0500

    🍃 File receiving logic

--- src/files.ts
+++ src/files.ts
@@ -1,30 +1,40 @@
 import { Composer } from 'https://deno.land/x/grammy@v1.20.3/mod.ts'
 import { MyContext } from './sessionsHandler.ts'
-
-await load({ export: true })
+import { OmnivoreApi } from './omnivore/api.ts'
 
 export const fileListener = new Composer<MyContext>()
 
 fileListener.on('message:document', async (ctx, next) => {
-  const pdf = ctx.msg.document
+  try {
 
-  const limit = 10_485_760
+    ctx.reply('Started processing your file')
+    
+    const file = ctx.msg.document
+    const fileType = file.mime_type
+    const fileSize = file.file_size
 
-  if (pdf.mime_type != 'application/pdf') {
-    return await next()
+    const limit = 10_485_760
+
+    if (fileType != 'application/pdf') {
+      return await next()
+    }
+
+    if (fileSize && fileSize > limit) {
+      return await ctx.reply(
+        'Your file size is exceeding our limit of 10mb 😿'
+      )
+    }
+
+    const fileInfo = await ctx.getFile()
+
+    const token = ctx.session.apiToken
+    const api = new OmnivoreApi(token)
+
+    await api.uploadFile(fileInfo, fileType)
+
+    ctx.reply(`File successfully uploaded`)
+  } catch (error) {
+    console.log(error)
+    ctx.reply('Something went wrong, please try again after a while')
   }
-  
-  if (pdf.file_size && pdf.file_size > limit) {
-    return await ctx.reply('Your file size is exceeding our limit of 10mb 😿')
-  }
-
-  const fileId = pdf.file_id
-  await ctx.reply(
-    'The file identifier of your voice message is: ' + fileId
-  )
-
-  const file = await ctx.getFile()
-  const path = file.file_path
-
-  const response = await fetch(`https://api.telegram.org/file/bot${Deno.env.get('BOT_TOKEN')}${path}`)
 })

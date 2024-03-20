08692e2a965f7e31069938bd018eabb3b1052992
Rnbsov <rnbsov@gmail.com>
2024-03-20 15:37:35 +0500

    🍃 Some code splitting

--- src/utils/getUrlAndLabels.ts
+++ src/utils/getUrlAndLabels.ts
@@ -1,55 +1,17 @@
-import { getSourceLabel } from "./getSourceLabel.ts";
-import { getDefaultLabel } from "./getDefaultLabel.ts";
-import { parseUrls } from "./parseUrls.ts";
-import { startsWithUrl } from "./startsWithUrl.ts";
-import { Label } from '../types.ts'
-import { type Filter } from "https://deno.land/x/grammy@v1.21.1/mod.ts"
+import { type Filter } from 'https://deno.land/x/grammy@v1.21.1/mod.ts'
 import { MyContext } from '../sessionsHandler.ts'
+import { getLabels } from "./getLabels.ts";
+import { retrieveUrl } from "./retrieveUrl.ts";
 
 export function getUrlAndLabels(ctx: Filter<MyContext, 'message:entities:url'>) {
-  let url, labels: Label[]
-  
-  // retrieving information from ctx 
-  const message = ctx.message.text
-  const source = ctx.msg?.forward_origin
-
-  // retrieving information from session 
-  const sessionIncludeSource = ctx.session.includeSource
-  const sessionDefaultLabel = ctx.session.defaultLabel
-
   // parse url from the message
-  if (startsWithUrl(message)) {
-    ;({ url, labels } = parseUrls(message)[0])
-  } else if (ctx.entities('text_link').length > 0) {
-    // handle case when user sends a message with text formatted link
-    const linkEntity = ctx.entities('text_link')[0]
-    if (linkEntity && linkEntity.url) {
-      url = linkEntity.url
-    }
-    labels = []
-  } else {
-    // retrieve the first url from the message/post
-    const urlEntity = ctx.entities('url')[0]
-    if (urlEntity && urlEntity.text) {
-      url = urlEntity.text
-    }
-    labels = []
-  }
+  const { url, labels: labelsArray } = retrieveUrl(ctx)
 
-  // add default label
-  const defaultLabel = getDefaultLabel(sessionDefaultLabel);
-  if (defaultLabel.name) {
-    labels.push(defaultLabel);
-  }
+  // add default label and source label
+  const defaultAndSourceLabels = getLabels(ctx)
 
-  // add source label if includeSource is true
-  if (source && sessionIncludeSource) {
-    const sourceLabel = getSourceLabel(source);
+  const labels = [...labelsArray, ...defaultAndSourceLabels]
 
-    if (sourceLabel) {
-      labels.push(sourceLabel);
-    }
-  }
-
-  return { url, labels };
+  return { url, labels }
 }
+
